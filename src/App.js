6b6ddf227b6d36ec716c23ded2c1a86fb62e8b62
Pierre Andre Bossetti Gutierrez <bossettp@MAC1059.groupemutuel.ch>
2024-03-23 11:10:39 +0100

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
+import Button from './Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+            Create your spotify playlist !
+        </h1>
+        <input type="search" id="site-search" name="q" />
+        <Button name="Search" />
+        <div className='container'>
+            <div className='searchedSongs'>
+
+            </div>
+            <div className='selectedSongs'>
+            <Button name="Save to Spotify" />
+            </div>
+        </div>
       </header>
     </div>
   );
